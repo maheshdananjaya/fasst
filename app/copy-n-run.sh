3a1a536e2f2ecd36f735a4ac125d189c663d538f
dananjayamahesh <dananjayamahesh@gmail.com>
2021-12-10 06:11:43 -0700

    add copy and ru script

---
+++ app/copy-n-run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Set LAT_WORKER to -1 to disable latency measurement or to worker id (i.e., from 0 up to [num-worker - 1])
+LAT_WORKER="-1"
+#LAT_WORKER="0"
+APP=tx-test
+EXEC_FOLDER="${HOME}/fasst/app/${APP}"
+REMOTE_COMMAND="cd ${EXEC_FOLDER}; bash run-app.sh"
+cd ${EXEC_FOLDER}
+
+# get Hosts
+source ./hosts.sh
+
+#../bin/copy-exec-files.sh
+                       ./run-app.sh ${args} &
+                 sleep 2 # give some leeway so that manager starts before executing the members
+	             parallel "echo running | ssh -tt {} $'${REMOTE_COMMAND}'" ::: $(echo ${REMOTE_HOSTS[@]}) >/dev/null
+cd - >/dev/null
+
+#../bin/get-system-xput-files.sh
