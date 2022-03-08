f6fbc0ee375f8282e99a5d05da0cd7bb26075b3f
mahesh <dananjayamahesh@gmail.com>
2022-03-08 14:56:52 +0000

    sb jason fixed

--- app/smallbank/run-servers.sh
+++ app/smallbank/run-servers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source $(dirname $0)/../../util/helpers.sh
 
-export HRD_REGISTRY_IP="128.110.96.117"
+export HRD_REGISTRY_IP="128.110.96.176"
 #export MLX5_DISABLE_BF=1
 export MLX5_SINGLE_THREADED=1
 export MLX4_SINGLE_THREADED=1
